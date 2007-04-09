6e109fbe7f58f930d58983786af876f961cf3768
Paul Fenwick <pjf@perltraining.com.au>
2007-04-09 06:26:07 +0000

    Bumped version to 0.05

--- lib/IPC/System/Simple.pm
+++ lib/IPC/System/Simple.pm
@@ -11,7 +11,7 @@ use POSIX qw(WIFEXITED WEXITSTATUS WIFSIGNALED WTERMSIG);
 require Exporter;
 our @ISA = qw(Exporter);
 our @EXPORT_OK = qw( run );
-our $VERSION = '0.04';
+our $VERSION = '0.05';
 
 my @Signal_from_number = split(' ', $Config{sig_name});
 
